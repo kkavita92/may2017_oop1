d24d6094b197b4e784c8530427bb385acee72d22
Kavita <kkavita92@gmail.com>
2017-05-11 11:41:11 +0100

    Adds tests for #Add method

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -5,7 +5,13 @@ describe Calculator do
   it 'adds two numbers together' do
     expect(calculator.add(2, 3)).to eq 5
     expect(calculator.add(3, 1)).to eq 4
+    expect(calculator.add(0, 1)).to eq 1
+    expect(calculator.add(-1, 2)).to eq 1
+    expect(calculator.add(-1, -1)).to eq -2
+    expect(calculator.add(0, Float::INFINITY)).to eq Float::INFINITY
+    expect(calculator.add(Float::INFINITY, Float::INFINITY)).to eq Float::INFINITY
   end
+
   it 'subtracts one number from another' do
     expect(calculator.subtract(2, 1)).to eq 1
     expect(calculator.subtract(5, 2)).to eq 3
