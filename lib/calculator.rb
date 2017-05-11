7c2007990ceff9d3d7a1b4739af8c5398819373b
Kavita <kkavita92@gmail.com>
2017-05-11 11:07:58 +0100

    Adds subtract method

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -11,4 +11,8 @@ class Calculator
   def add(number_1, number_2)
     number_1 + number_2
   end
+
+  def subtract(number, number_to_subtract)
+    number - number_to_subtract 
+  end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -25,7 +25,10 @@ RSpec.describe 'a calculator' do
       expect(subject.add(2, 3)).to eq 5
       expect(subject.add(3, 1)).to eq 4
     end
+
+    it 'subtracts one number from another' do
+      expect(subject.subtract(2, 1)).to eq 1
+      expect(subject.subtract(5, 2)).to eq 3
+    end
   end
 end
-
-
